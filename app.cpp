142a80f8f419f73f6e9e729fc83c9ac22de0c5ef
haroonarfan <haroonarfan109@gmail.com>
2020-07-14 00:51:28 +0000

    D

---
+++ app.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main() {
+    int num;
+    cout << "Enter your Number : ";
+    cin >> num;
+    cout << "Your number is :" << num << endl;
+    string name;
+    cout << "Input your name: ";
+    cin >> name;
+    cout << "Done ✔ " << name;
+    return 0;
+}
