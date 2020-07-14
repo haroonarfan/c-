2bdd9be679f07100841694227d58845bda1f91f0
haroonarfan <haroonarfan109@gmail.com>
2020-07-14 19:50:58 +0000

    LCM & HCF

--- app.cpp
+++ app.cpp
@@ -1,16 +1,42 @@
+// Creating LCM and HCF Calculator
 #include <iostream>
-#include <string>
 
 using namespace std;
 
+// Functions
+void HCF(int num1,int  num2);
+void LCM(int num1, int num2);
+
 int main() {
-    int num;
-    cout << "Enter your Number : ";
-    cin >> num;
-    cout << "Your number is :" << num << endl;
-    string name;
-    cout << "Input your name: ";
-    cin >> name;
-    cout << "Done ✔ " << name;
+    int num1, num2;
+    cout << "Ener 2 numbers: ";
+    cin >> num1 >> num2;
+    HCF(num1,num2);
+    LCM(num1, num2);
     return 0;
+}
+
+void HCF(int num1,int  num2) {
+    int Factor {1};
+    for (int i = 1; i <= (num1*(num1>num2)+num2*(num1<num2))/2; ++i) {
+        if ((num1 % i == 0) == true && (num2 % i == 0) == true) {
+            Factor = i;    
+        }
+    }
+    cout << "The HCF is: " << Factor << endl;
+    return;
+}
+
+void LCM(int num1, int num2) {
+    int Multiple {1}, Number1 {num1}, Number2 {num2};
+    for (int i {1}; true; ++i) {
+        if (Number1 == Number2) {
+            cout << "The LCM is: " << Number1 << endl;
+            break;
+        } else if (Number1 > Number2) {
+            Number2 = num2 * i;
+        } else if (Number1 < Number2) {
+            Number1 = num1 * i;
+        }
+    }
 }
