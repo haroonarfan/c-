3ba058f062433d5d761ccc8e71e73b84f7146412
haroonarfan <haroonarfan109@gmail.com>
2020-07-15 18:52:58 +0000

    HCF project completed

--- app_improved.cpp
+++ app_improved.cpp
@@ -21,15 +21,23 @@ int main() {
     while (os >> Numbers)
         myVector.push_back(Numbers);
 
-    cout << HCF(myVector);
+    cout << "The HCF is: " << HCF(myVector) << endl;
 
   return 0;
 }
 
 int HCF(vector<int> numbers) {
-    int Factor {1}, max {*max_element(numbers.begin(), numbers.end())};
+    int Factor {1}, max {*max_element(numbers.begin(), numbers.end())}, count {0};
     for (int i = 1; i <= max/2; ++i) {
-        
+        for (int j:numbers) {
+            if (j%i==0) {
+                count++;
+            }
+        }
+        if (count==numbers.size() ){
+            Factor = i;
+        }
+        count = 0;
     }
-    return max;
+    return Factor;
 }
